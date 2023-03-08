e0d3259b8f31ed73b92ca0645ed098dec7d6fe58
Claudio <claudio.lammirato@gmail.com>
2023-03-08 17:02:03 +0100

    first commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "wirelessXbee.h"
+
+float temp_ext;
+float hum_ext;
+float temp_int;
+
+int main() {
+    temp_ext = 25.6;
+    hum_ext = 50;
+    temp_int = 22;
+
+    writeXbee(temp_ext, hum_ext, temp_int);
+    return 0;
+}
---
+++ wirelessXbee.h
@@ -0,0 +1,7 @@
+#ifndef wirelessXbee_h
+#define wirelessXbee_h
+
+void writeXbee(float temp_ext, float hum_ext, float temp_int);
+
+
+#endif
