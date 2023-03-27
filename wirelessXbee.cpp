b08db85bba61cb6f3f68fa76a69320a0300b8e2f
Claudio <claudio.lammirato@gmail.com>
2023-03-27 19:27:33 +0200

    v0.5

--- wirelessXbee.cpp
+++ wirelessXbee.cpp
@@ -12,41 +12,41 @@ void writeXbee(std::string str, std::string& msg_out, std::string addresslow){
     msg_out = msg_out + '7'; // Start Delimeter
     msg_out = msg_out + 'E'; // Start Delimeter
     //1 byte
-    msg_out = msg_out + '0'; // Start Delimeter
-    msg_out = msg_out + '0'; // Start Delimeter
+    msg_out = msg_out + '0'; // Plceholder length
+    msg_out = msg_out + '0'; // Plceholder length
     //2 byte placeholder - at the end will be calculated the lenght in hex
-    msg_out = msg_out + '0'; // Start Delimeter
-    msg_out = msg_out + '0'; // Start Delimeter
+    msg_out = msg_out + '0'; // Plceholder length
+    msg_out = msg_out + '0'; /// Plceholder length
     //3 byte
-    msg_out = msg_out + '1'; // Start Delimeter
-    msg_out = msg_out + '0'; // Start Delimeter
+    msg_out = msg_out + '1'; // Frame Type
+    msg_out = msg_out + '0'; // Frame Type
     //4 byte
-    msg_out = msg_out + '0'; // Start Delimeter
-    msg_out = msg_out + '1'; // Start Delimeter
+    msg_out = msg_out + '0'; //Frame ID
+    msg_out = msg_out + '1'; //Frame ID
     //5 byte
-    msg_out = msg_out + '0'; // Start Delimeter
-    msg_out = msg_out + '0'; // Start Delimeter
+    msg_out = msg_out + '0'; //Address HIGH
+    msg_out = msg_out + '0'; //Address HIGH
     //6 byte
-    msg_out = msg_out + '1'; // Start Delimeter
-    msg_out = msg_out + '3'; // Start Delimeter
+    msg_out = msg_out + '1'; //Address HIGH
+    msg_out = msg_out + '3'; //Address HIGH
     //7 byte
-    msg_out = msg_out + 'A'; // Start Delimeter
-    msg_out = msg_out + '2'; // Start Delimeter
+    msg_out = msg_out + 'A'; //Address HIGH
+    msg_out = msg_out + '2'; //Address HIGH
     //8 byte
-    msg_out = msg_out + '0'; // Start Delimeter
-    msg_out = msg_out + '0'; // Start Delimeter
+    msg_out = msg_out + '0'; //Address HIGH
+    msg_out = msg_out + '0'; //Address HIGH
     
-    //from 9 to 12 byte
+    //from 9 to 12 byte - Address LOW
     for(int i = 0; i<addresslow.length(); i++){
         msg_out = msg_out + addresslow[i];
     }
 
     //13 byte
-    msg_out = msg_out + 'F'; // Start Delimeter
-    msg_out = msg_out + 'F'; // Start Delimeter
+    msg_out = msg_out + 'F'; //16 Bit Destination address
+    msg_out = msg_out + 'F'; //16 Bit Destination address
     //14 byte
-    msg_out = msg_out + 'F'; // Start Delimeter
-    msg_out = msg_out + 'E'; // Start Delimeter
+    msg_out = msg_out + 'F'; //Broadcast Radius + Options
+    msg_out = msg_out + 'E'; //Broadcast Radius + Options
     //15 byte
     msg_out = msg_out + '0'; // Start Delimeter
     msg_out = msg_out + '0'; // Start Delimeter
@@ -64,7 +64,7 @@ void writeXbee(std::string str, std::string& msg_out, std::string addresslow){
     checksum = checksum.substr(1,2); 
     checksum = hexmin("FF" , checksum);
 
-    msg_out = msg_out + checksum;
+    msg_out = msg_out + checksum; // Checksum
     
     //Length calculation
     l = (msg_out.length()-8)/2;
@@ -74,8 +74,8 @@ void writeXbee(std::string str, std::string& msg_out, std::string addresslow){
     std::string result( stream.str() );
     //std::cout << result <<'\n';
 
-    msg_out[4] = result[0];
-    msg_out[5] = result[1];
+    msg_out[4] = result[0]; // Change with right length
+    msg_out[5] = result[1]; // Change with right length
 }
 
 
